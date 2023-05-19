bcbfdc2b0b2d4000f8d3ad739a6bc535d5768786
Oleg <zolegzz@yandex.ru>
2023-05-20 01:13:37 +0200

    add pagination

--- data/postitions_3.py
+++ data/postitions_3.py
@@ -3,34 +3,30 @@ from pydantic import BaseModel
 from .contracts import collaterals, controllers, stablecoin
 
 
-class ControllerPosition(BaseModel):
-    address: str
-    collateral: str
+def get_controllers() -> dict:
+    return {
+        col.symbol: {"controller": controllers[col_addr].address, "collateral": col.address}
+        for col_addr, col in collaterals.items()
+    }
+
+
+class Positions(BaseModel):
+    n_loans: int
     positions: list[tuple]
 
 
-def get_positions() -> list[ControllerPosition]:
-    controller_positions = []
+def get_positions(collateral_address: str, pagination: int = 10, page: int = 1) -> Positions:
+    col = collaterals[collateral_address]
+    controller = controllers[collateral_address]
+    n_loans = controller.n_loans()
+    positions = []
 
-    for col_addr, col in collaterals.items():
-        collateral_symbol = col.symbol
-        controller = controllers[col_addr]
-        positions = []
+    for i in range((page - 1) * pagination, min(n_loans, page * pagination + 1)):
+        user = controller.loans(i)
+        user_state = controller.user_state(user)
+        user_state[0] = user_state[0] / col.precision
+        user_state[1] = user_state[1] / stablecoin.precision
+        user_state[2] = user_state[2] / stablecoin.precision
+        positions.append((i, user, *user_state))
 
-        for i in range(controller.n_loans()):
-            user = controller.loans(i)
-            user_state = controller.user_state(user)
-            user_state[0] = user_state[0] / col.precision
-            user_state[1] = user_state[1] / stablecoin.precision
-            user_state[2] = user_state[2] / stablecoin.precision
-            positions.append((user, *user_state))
-
-        controller_positions.append(
-            ControllerPosition(
-                address=controller.address,
-                collateral=collateral_symbol,
-                positions=positions,
-            )
-        )
-
-    return controller_positions
+    return Positions(n_loans=n_loans, positions=positions)
--- pages/3_💳_Postitions.py
+++ pages/3_💳_Postitions.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 
-from data.postitions_3 import get_positions
+from data.postitions_3 import get_controllers, get_positions
 
 # Config
 st.set_page_config(page_title="Positions", page_icon="💳", layout="wide")
@@ -10,9 +10,50 @@ st.set_page_config(page_title="Positions", page_icon="💳", layout="wide")
 st.title("Positions")
 
 # Positions
-positions = get_positions()
+controllers = get_controllers()
+if "pages" not in st.session_state:
+    st.session_state.pages = {}
 
-for controller in positions:
-    st.write(f"Positions for {controller.collateral} [Controller](https://etherscan.io/address/{controller.address}):")
-    df = pd.DataFrame(controller.positions, columns=["user", "collateral", "stablecoin", "debt", "N"])
+
+def set_page(col_symbol: str, page: int):
+    st.session_state.pages[col_symbol] = page
+
+
+for col_symbol in controllers:
+    if col_symbol not in st.session_state.pages:
+        set_page(col_symbol, page=1)
+
+    page = st.session_state.pages[col_symbol]
+    pagination = 10
+
+    st.write(
+        f"Positions for {col_symbol} [Controller](https://etherscan.io/address/{controllers[col_symbol]['controller']}):"
+    )
+    positions = get_positions(controllers[col_symbol]["collateral"], pagination=pagination, page=page)
+    df = pd.DataFrame(positions.positions, columns=["n", "user", "collateral", "stablecoin", "debt", "N"])
     st.table(df.astype(str))
+
+    col1, col2, col3, col4 = st.columns(4)
+
+    with col1:
+        st.button(
+            "Previous", on_click=set_page, kwargs={"col_symbol": col_symbol, "page": page - 1}, disabled=page == 1
+        )
+
+    with col2:
+        st.button("1", on_click=set_page, kwargs={"col_symbol": col_symbol, "page": 1}, disabled=page == 1)
+
+    with col3:
+        st.button(
+            "Last",
+            on_click=set_page,
+            kwargs={"col_symbol": col_symbol, "page": positions.n_loans // pagination + 1},
+            disabled=page * pagination > positions.n_loans,
+        )
+    with col4:
+        st.button(
+            "Next",
+            on_click=set_page,
+            kwargs={"col_symbol": col_symbol, "page": page + 1},
+            disabled=page * pagination > positions.n_loans,
+        )
